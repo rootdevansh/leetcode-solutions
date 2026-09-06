4e81e0157a66ab1e8fafea8972d41a3e1539ca40
Devansh Bhardwaj <devanshbhardwaj23@yahoo.com>
2026-09-06 11:11:23 +0530

    Time: 0 ms (100%), Space: 13.4 MB (74.95%) - LeetHub

---
+++ 1848-minimum-distance-to-the-target-element/1848-minimum-distance-to-the-target-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int getMinDistance(vector<int>& nums, int target, int start) {
+        int mini=INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==target){
+                mini=min(abs(i-start),mini);
+            }
+        }
+        return mini;
+    }
+};
